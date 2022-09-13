b51bdd0ebae8a6e166cda7e29a51eed71fcc0c79
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-13 16:59:28 +0900

    install and set up ex_doc

--- mix.exs
+++ mix.exs
@@ -1,14 +1,22 @@
 defmodule OpenblasBuilder.MixProject do
   use Mix.Project
 
+  @version "0.1.0-dev"
+
   def project do
     [
       app: :openblas_builder,
-      version: "0.1.0-dev",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      compilers: Mix.compilers() ++ [:elixir_make]
+      compilers: Mix.compilers() ++ [:elixir_make],
+      docs: [
+        main: "readme",
+        source_url: "https://github.com/zeam-vm/openblas_builder",
+        source_ref: "v#{@version}",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -24,7 +32,8 @@ defmodule OpenblasBuilder.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:elixir_make, "~> 0.6", runtime: false}
+      {:elixir_make, "~> 0.6", runtime: false},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
 end
