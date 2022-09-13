2b6304ccf9a03b65ab9e3015bbc10023a8f16632
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-13 17:14:40 +0900

    add network_tool

--- lib/openblas_builder.ex
+++ lib/openblas_builder.ex
@@ -2,4 +2,19 @@ defmodule OpenBLASBuilder do
   @moduledoc """
   Documentation for `OpenBLASBuilder`.
   """
+
+  @doc """
+  Returns `:curl` or `:wget` if found in the PATH.
+
+  Return `nil` if not found.
+  """
+  def network_tool() do
+    cond do
+      executable_exists?("curl") -> :curl
+      executable_exists?("wget") -> :wget
+      true -> nil
+    end
+  end
+
+  defp executable_exists?(name), do: System.find_executable(name) != nil
 end
