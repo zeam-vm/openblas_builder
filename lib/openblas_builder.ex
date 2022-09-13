d147e4aeaf3b7aa1e6f9113038b2ab2c05c79d44
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-13 16:55:18 +0900

    remove hello

--- lib/openblas_builder.ex
+++ lib/openblas_builder.ex
@@ -2,17 +2,4 @@ defmodule OpenBLASBuilder do
   @moduledoc """
   Documentation for `OpenBLASBuilder`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> OpenBLASBuilder.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
